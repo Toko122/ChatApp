496b6b69dc4348a63019dc52e6f47f8835a3c038
Toko Migineishvili <toko.migine@gmail.com>
2026-01-05 14:54:32 +0400

    Update Auth.tsx

--- chatapp/src/lib/Auth.tsx
+++ chatapp/src/lib/Auth.tsx
@@ -72,4 +72,13 @@ const Auth = ({children}: AuthProviderProps) => {
 
 export default Auth
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+   const context = useContext(AuthContext)
+
+   if (!context) {
+    throw new Error('useAuth must be used inside AuthProvider')
+  }
+
+  return context
+
+}
